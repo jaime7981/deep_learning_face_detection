d0c0f1238fe6ce2e28ee61effacbe88a653c8caf
jaime7981 <jmfernandez1@miaundes.cl>
2023-10-21 06:02:57 -0300

    main program

---
+++ main.py
@@ -0,0 +1,11 @@
+import dataset.generate_dataset
+import retinaface.retinaface.RetinaFace
+import retinaface.retinaface.model.retinaface_model
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
